1d190414eec42be8a062c0e0fd8ae13cb5e4bd0d
Federico Prati <FEDERICO.PRATI@local.ittcesena.it>
2024-03-02 11:44:08 +0100

    riempimento repository

--- Codifiche_Ascii/Codifiche_Ascii/obj/Debug/net6.0/Codifiche_Ascii.AssemblyInfo.cs
+++ Codifiche_Ascii/Codifiche_Ascii/obj/Debug/net6.0/Codifiche_Ascii.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3b01484a7bd5c37d21fce4f49ca810edf5597cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
